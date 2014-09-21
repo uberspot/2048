04a6d412711a29d387784826ee5480aa060eb3a5
uberspot <onexemailx@gmail.com>
2014-09-21 19:01:52 +0300

    Optimization for tileMatchesAvailable (credits to kingjiv)

--- js/game_manager.js
+++ js/game_manager.js
@@ -260,7 +260,7 @@ GameManager.prototype.tileMatchesAvailable = function () {
       tile = this.grid.cellContent({ x: x, y: y });
 
       if (tile) {
-        for (var direction = 0; direction < 4; direction++) {
+        for (var direction = 0; direction < 2; direction++) {
           var vector = self.getVector(direction);
           var cell   = { x: x + vector.x, y: y + vector.y };
 
